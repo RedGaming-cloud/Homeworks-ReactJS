7456aea079b3ebc02c626feb4cab288c87a342ac
RedGaming-cloud <79407143+RedGaming-cloud@users.noreply.github.com>
2021-08-05 22:42:55 +0300

    Update App.js

--- lesson1_hw/src/App.js
+++ lesson1_hw/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>       
-        <h4 className="message"> { Message(Text) } </h4>
+        <h4 className="message"> { Message text = { Text } } </h4>
       </header>
     </div>
   );
